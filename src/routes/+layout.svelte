574beb3852db1822bee07e1ae486ecf7b384bf79
Shalane Proctor <94003497+shalane-proctor@users.noreply.github.com>
2024-06-19 00:33:26 -0500

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 <svelte:head>
 	<link rel="icon" type="image/svg" href={"/logo2.svg"} />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/smui.css" />
 </svelte:head>
 
 <div class="app">
