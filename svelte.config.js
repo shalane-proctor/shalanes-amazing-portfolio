f09b42e2e061996897fe2a41bde7e98dff38c2f0
Shalane Proctor <94003497+shalane-proctor@users.noreply.github.com>
2024-06-19 00:05:11 -0500

    updated to try to fix deploy issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,2 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import pkg from './package.json';
-
-export default {
-	kit: {
-		adapter: adapter(),
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	},
-	preprocess: preprocess()
-};
-
+export default {	kit: {		adapter: adapter({		})	}};
