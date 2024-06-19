c4ecbb1d0ea1904a6ca99543e414affa8b315f18
Shalane Proctor <94003497+shalane-proctor@users.noreply.github.com>
2024-06-19 00:50:58 -0500

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
 export default {	kit: {		adapter: adapter({
-			runtime: 'nodejs20.x'
+			runtime: 'nodejs20.x',
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['shalanes-amazing-portfolio.vercel.app'],
+			},
+
 })
 }};
