0eafa234bf882c31d38413ebe27694451a584403
Shalane Proctor <94003497+shalane-proctor@users.noreply.github.com>
2024-06-20 18:14:28 -0500

    Updated vite onfig and svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-vercel';
-export default {	kit: {		adapter: adapter({
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	kit: {
+		adapter: adapter({
 			runtime: 'nodejs20.x',
 			images: {
 				sizes: [640, 828, 1200, 1920, 3840],
 				formats: ['image/avif', 'image/webp'],
 				minimumCacheTTL: 300,
-				domains: ['shalanes-amazing-portfolio.vercel.app'],
-			},
+				domains: ['shalanes-amazing-portfolio.vercel.app']
+			}
+		})
+	},
+	preprocess: vitePreprocess(),
+};
 
-})
-}};
+export default config;
